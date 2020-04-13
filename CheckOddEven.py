fea45f5c4a0b472971a3a600ddc84f67508b8915
Jaiman Deshpande <jaiman.deshpande@gmail.com>
2020-04-13 20:06:45 +0530

    added loop and learnt different data types

--- CheckOddEven.py
+++ CheckOddEven.py
@@ -1,6 +1,8 @@
-a = 10
-if a%2 == 0:
-	print("a is even number")
-else:
-	print("a is odd number")
-	
+a = input("Enter the number")
+while(a != 'q'):
+	a = int(a)
+	if a%2 == 0:
+		print("a is even number")
+	else:
+		print("a is odd number")
+	a = input("Enter the number")
